0524540ed418a56dd01ae7392c9f550bb8ae4062
Oliver Lee <oliverzlee@gmail.com>
2021-10-10 17:40:48 +0200

    ci: Add Bazel setup script

--- run_format.sh
+++ run_format.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -eux
+
 format_bin=${1:-/usr/local/opt/llvm/bin/clang-format}
 buildifier_bin=${2:-/usr/local/bin/buildifier}
+
 find .  \( -name "*.cpp" -o -name "*.hpp" \) -exec ${format_bin} -i {} \;
 find .  \( -name "*BUILD" -o -name "WORKSPACE" -o -name "*.bzl" \) | \
     xargs ${buildifier_bin}
