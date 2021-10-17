8d082663ed41e4dfdf06329b3c8043aa644e5e59
Oliver Lee <oliverzlee@gmail.com>
2021-10-17 16:25:35 +0200

    Update example to rolling disc

--- example/example.cpp
+++ example/example.cpp
@@ -2,43 +2,46 @@
 
 #include "fmt/core.h"
 
-#include <numbers>
-
 auto main() -> int
 {
-    using std::numbers::pi;
     using turtle::frame;
     using turtle::orientation;
     using turtle::world;
 
-    using N = frame<"N">;
-    using A = frame<"A">;
-    using B = frame<"B">;
+    using N = frame<"Inertial">;
+    using Y = frame<"Yaw">;
+    using L = frame<"Lean">;
+    using R = frame<"Roll">;
 
-    const auto w = world{orientation<N, A>{pi / 2., N::vector{1., 0., 0.}},
-                         orientation<N, B>{-pi / 6., N::vector{0., 0., 1.}}};
-    fmt::print("using world w:\n{}\n", w);
+    constexpr auto make_world =
+        [](N::scalar q1 = {},
+           N::scalar q2 = {},
+           N::scalar q3 = {},
+           N::scalar u1 = {},
+           N::scalar u2 = {},
+           N::scalar u3 = {}) {
+            return world{
+                orientation<N, Y>{q1, N::z},
+                orientation<Y, L>{q2, Y::x},
+                orientation<L, R>{q3, L::y}.with(L::velocity{u1, u2, u3}),
+            };
+        };
 
-    constexpr auto r1 = A::position{1., 2., 3.};
-    fmt::print("position {} in frame {} is {}\n", r1, B{}, r1.in<B>(w));
+    using P = decltype(make_world())::point;
+    auto dmc = P{L::position{0, 0, 1}};
+    dmc.velocity(R::velocity{});
 
-    using P = decltype(w)::point;
+    {
+        const auto w = make_world(0.1, 0.2, 0.3, 2, 3, 4);
+        fmt::print("using world w:\n{}\n", w);
+        fmt::print("vel of disc mass center: {:.2f}\n", dmc.velocity<N>(w));
+        fmt::print("vel of disc mass center: {:.2f}\n", dmc.velocity<N, L>(w));
+    }
 
-    constexpr auto p = P{};
-    fmt::print("\ncreating a point p at w's origin {}\n", p);
-    fmt::print("expressing p in {}\n", p.position<B>(w));
-
-    auto q = P{A::position{0., 2., 1.}};
-    fmt::print("\nand a point q {}\n", q);
-    fmt::print("expressing in {}\n", q.position<B>(w));
-
-    constexpr auto v1 = N::velocity{3, 0, 0};
-    fmt::print("v1: {:.2f}\n", v1);
-
-    constexpr auto v2 = turtle::velocity<B, A>{3, 0, 0};
-    fmt::print("v2: {:.2f}\n", v2);
-
-    q.velocity(v2);
-
-    return 0;
+    {
+        const auto w = make_world(1, 2, 3, 2, 3, 4);
+        fmt::print("\n\nusing world w:\n{}\n", w);
+        fmt::print("vel of disc mass center: {:.2f}\n", dmc.velocity<N>(w));
+        fmt::print("vel of disc mass center: {:.2f}\n", dmc.velocity<N, L>(w));
+    }
 }
